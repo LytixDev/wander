a49a59bd2e5017e716f1287e768fd84db8026d13
Nicolai Brand <nicolahb@stud.ntnu.no>
2023-05-09 20:47:39 +0200

    init

---
+++ Makefile
@@ -0,0 +1,42 @@
+# Nicolai Brand (lytix.dev) 2022-2023
+# See LICENSE for license info
+
+OBJDIR = .obj
+SRC = src
+DIRS := $(shell find $(SRC) -type d)
+SRCS := $(shell find $(SRC) -type f -name "*.c")
+OBJS := $(SRCS:%.c=$(OBJDIR)/%.o)
+
+CC = gcc
+CFLAGS = -I include -Wall -Wpedantic -Wextra -Wshadow -std=c11
+
+.PHONY: clean tags bear $(OBJDIR)
+TARGET = yopierre
+
+all: $(TARGET)
+
+$(OBJDIR)/%.o: %.c Makefile | $(OBJDIR)
+	@echo [CC] $@
+	@$(CC) -c $(CFLAGS) -c $< -o $@
+
+$(TARGET): $(OBJS)
+	@echo [LD] $@
+	@$(CC) -o $@ $^
+
+debug: CFLAGS += -g -DDEBUG
+debug: $(TARGET)
+
+debug-verbose: CFLAGS += -DDEBUG_VERBOSE
+debug-verbose: debug
+
+clean:
+	@rm -rf $(OBJDIR) $(TARGET)
+
+tags:
+	@ctags -R
+
+bear:
+	bear -- make
+
+$(OBJDIR):
+	$(foreach dir, $(DIRS), $(shell mkdir -p $(OBJDIR)/$(dir)))
---
+++ tests/run.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Copyright (C) 2023 Nicolai Brand (https://lytix.dev)
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+successes=0
+mem_leak_option="$1"
+n_tests="$(ls ./*.c | wc -l)"
+
+for file in *.c
+do
+    gcc -g -fsanitize=address -fsanitize=undefined -Wall -Wpedantic -Wextra -Werror "$file" ../hashmap.c
+    [ "$mem_leak_option" = "-m" ] && (valgrind -s ./a.out >/dev/null)
+    if ./a.out
+    then
+        printf "\033[0;32m$file PASSED\033[0m\n\n"
+        successes=$((successes + 1))
+    else
+        printf "\033[0;31m$file FAILED\033[0m\n\n"
+    fi
+done
+
+rm a.out
+
+echo "$successes" / "$n_tests" tests passed
