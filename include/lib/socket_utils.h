5c7bf86e17762421833eeb93cb4ec5bbc338805c
callumgran <callum.gran@gmail.com>
2023-05-15 14:03:31 +0200

    added nonblocking function

---
+++ include/lib/socket_utils.h
@@ -0,0 +1,27 @@
+/*
+ *  Copyright (C) 2023 Nicolai Brand, Callum Gran
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef SOCKET_UTILS_H
+#define SOCKET_UTILS_H
+
+/**
+ * Sets the socket to non-blocking mode.
+ * @param socket The socket to set to non-blocking mode.
+ */
+void set_nonblocking(int socket);
+
+#endif /* SOCKET_UTILS_H */
