0799ce89613b82ee33a802f99ab4ab789dc78c4f
callumgran <callum.gran@gmail.com>
2023-05-16 16:29:10 +0200

    fixed __fn__

--- include/lib/lambda.h
+++ include/lib/lambda.h
@@ -26,6 +26,6 @@
  * @return A pointer to the lambda function.
  */
 #define LAMBDA(return_type, func_args, func_body) \
-    ({ return_type __fn_t__ func_args func_body &__fn_t__; })
+    ({ return_type __fn__ func_args func_body &__fn__; })
 
 #endif
