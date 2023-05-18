94b3fcafc065924fe59247e3089b1840809670d9
Nicolai Brand <nicolahb@stud.ntnu.no>
2023-05-18 17:39:26 +0200

    fix: various mem leaks (#29)
    * fix various memory leaks
    
    * rename

--- src/ulsr/packet.c
+++ src/ulsr/packet.c
@@ -22,27 +22,17 @@
 #include "lib/common.h"
 #include "ulsr/packet.h"
 
-struct ulsr_internal_packet *ulsr_internal_packet_new(struct ulsr_packet *external_packet)
+struct ulsr_internal_packet *ulsr_internal_from_external(struct ulsr_packet *external)
 {
     struct ulsr_internal_packet *packet = malloc(sizeof(struct ulsr_internal_packet));
     packet->payload_len = sizeof(struct ulsr_packet);
-    packet->payload = external_packet;
+    packet->payload = external;
     packet->prev_node_id = 0;
     packet->dest_node_id = 0;
     packet->type = PACKET_DATA;
     return packet;
 }
 
-// u32 ulsr_checksum(struct ulsr_packet *packet)
-//{
-//     u32 checksum = 0;
-//
-//     /* i starts at 4 because we don't want to include the checksum in the checksum calculation */
-//     for (unsigned long i = 4; i < sizeof(struct ulsr_packet); i++)
-//	checksum += ((u8 *)packet)[i];
-//
-//     return ~checksum;
-// }
 
 u32 ulsr_checksum(u8 *packet, unsigned long size)
 {
