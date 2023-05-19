31bda37afaadd2e87135c336a58b1980af552993
Nicolai Brand <nicolahb@stud.ntnu.no>
2023-05-19 22:00:11 +0200

    start work on some edge cases

--- include/ulsr/packet.h
+++ include/ulsr/packet.h
@@ -24,7 +24,7 @@
 enum ulsr_packet_type {
     ULSR_HTTP,
     ULSR_RESPONSE,
-    ULSR_FAILURE,
+    ULSR_INTERNAL_FAILURE,
 };
 
 struct ulsr_packet {
@@ -80,6 +80,8 @@ struct ulsr_packet *ulsr_create_response(struct ulsr_packet *packet, u8 *respons
  * Allocates the internal packet on the heap.
  */
 struct ulsr_internal_packet *ulsr_internal_from_external(struct ulsr_packet *external_packet);
+
+struct ulsr_packet *ulsr_create_failure(struct ulsr_packet *packet_that_failed);
 
 struct ulsr_internal_packet *ulsr_internal_create_hello(u16 from, u16 to);
 
